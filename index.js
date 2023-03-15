53885e8ccc6bdfcabf57d0a7c830d19a9d0181b9
SpecTrail5 <elijahiwatson2003@gmail.com>
2023-03-15 00:39:30 +0000

    added calc rotation

--- index.js
+++ index.js
@@ -1,6 +1,35 @@
 (function (window, _) {
   window.idk = window.idk || {
-    numz: {},
+    numz: {
+      /**
+         This function calculates the distnace between two points
+         * @param {object} pointA
+         * @param {object} pointB
+         * @param {number} pointA.x
+         * @param {number} pointA.y
+         * @param {number} pointB.x
+         * @param {number} pointB.y
+         */
+         getAngleDegrees(pointA, pointB){
+          const
+                distanceX = pointB.x - pointA.x,
+                distanceY = pointB.y - pointA.y,
+                radians = Math.atan2(distanceY, distanceX),
+                degrees = radians * 180 / Math.PI;
+          return degrees;
+      },
+
+     /** @param {number} degrees */ 
+      degreesToRadians(degrees) {
+        return degrees * Math.PI / 180;
+      },
+
+      /**@param {number} radians */
+      radiansToDegrees(radians) {
+        return radians * 180 / Math.PI;
+      },
+
+    },
     phyz: {
        /**
        * 
